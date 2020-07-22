7222a80093ee463082c9f277cbc3419e7fea7a14
Khoi Hoang <hello@koicha.dev>
2020-07-22 21:22:09 +0200

    GUI: :lipstick: Added more dynamic style for the page sites
    Every page sites will be relatively dynamic on based from the page name of the title and content

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Gives WP control for the head section, such as installing multiple plugins -->
     <?php wp_head(); ?>
-    <title>Document</title>
+    <title></title>
 </head>
 <body>
 
