b78483e11347c526d76dedb24f2f65e1a596c8af
Khoi Hoang <hello@koicha.dev>
2020-07-22 21:45:20 +0200

    GUI: :lipstick: Added the Breadcrumbs for pages
    The breadcrumbs is dynamically based on where user is going visit the website.

--- page.php
+++ page.php
@@ -17,8 +17,15 @@
         <div class="container container--narrow page-section">
 
             <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-            </div>
+            <?php $theParent = wp_get_post_parent_id(get_the_ID()); ?>
+            <?php if($theParent) : ?>
+            <p>
+                <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>">
+                    <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?>
+                </a> <span class="metabox__main"><?php the_title(); ?></span>
+            </p>
+            <?php endif; ?>
+        </div>
             
             <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
