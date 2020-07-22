4a636a602823bb44c966c3380f122acfe572e706
Khoi Hoang <hello@koicha.dev>
2020-07-22 19:16:57 +0200

    Add: :sparkles: :lipstick: Added new files for displaying static HTML for index.php
    Added files to display GUI on the front page

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@
 
     function kho_university_files() {
         // Load CSS file
+        wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+        wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         // get_stylesheet_uriRetrieves the URI of current theme stylesheet. (WordPress Snippets)
         wp_enqueue_style('kho_university_styles', get_stylesheet_uri());
     } 
