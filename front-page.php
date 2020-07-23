125930f1f5441f82ec2cd48faff9526935aef784
Khoi Hoang <hello@koicha.dev>
2020-07-23 16:33:01 +0200

    Add: :sparkles: :lipstick: Added custom query based on query we want on the front page site
    The front page will not only display recent  2 newest post based on our custom query we created from the WP_Query instance class

--- front-page.php
+++ front-page.php
@@ -43,28 +43,35 @@
     <div class="full-width-split__two">
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+        <?php 
+          $homePagePosts = new WP_Query([
+            'posts_per_page' => 2
+            // 'category_name' => 'award', // This will give the specific category name 
+            // 'post_type' => 'page' // This will give all page that exist 
+          ]);
+           
 
-        <div class="event-summary">
-          <a class="event-summary__date event-summary__date--beige t-center" href="#">
-            <span class="event-summary__month">Jan</span>
-            <span class="event-summary__day">20</span>  
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
-            <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
-          </div>
-        </div>
-        <div class="event-summary">
-          <a class="event-summary__date event-summary__date--beige t-center" href="#">
-            <span class="event-summary__month">Feb</span>
-            <span class="event-summary__day">04</span>  
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-            <p>Two of our processors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-          </div>
-        </div>
-        
+          while($homePagePosts -> have_posts()) {
+            $homePagePosts -> the_post();
+            ?>
+            <div class="event-summary">
+              <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                <span class="event-summary__month"><?php the_time('M'); ?></span>
+                <span class="event-summary__day"><?php the_time('d'); ?></span>  
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+              </div>
+            </div>
+            <?php
+          } 
+          // This will reset different WP data and global variables back to the default old state that 
+          // it was when customizing our own query of $homePagePosts 
+          // it's default automatically query URL
+          // ! Get used to this habit! It's important even it's not necessarily!
+          wp_reset_postdata();
+        ?>        
         <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
     </div>
