65385199a84d2aa525b171ffdc61f8b87560d3c5
Khoi Hoang <hello@koicha.dev>
2020-07-23 16:42:58 +0200

    Add: :sparkles: Added correct href:link for view all blog post on the front page
    It will direct to our blog page

--- front-page.php
+++ front-page.php
@@ -72,7 +72,7 @@
           // ! Get used to this habit! It's important even it's not necessarily!
           wp_reset_postdata();
         ?>        
-        <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+        <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
     </div>
   </div>
