a4d7ebebbe28f31f9cfa753222542145b3ebb925
Khoi Hoang <hello@koicha.dev>
2020-07-23 22:11:33 +0200

    GUI: :lipstick: Added Excerpt to display or the front page or not

--- front-page.php
+++ front-page.php
@@ -34,7 +34,13 @@
                 <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                 </h5>
                 <p>
-                <?php echo wp_trim_words(get_the_content(), 18) ?>
+                 <?php 
+                if(has_excerpt()) {
+                  echo get_the_excerpt();
+                  } else {
+                    echo wp_trim_words(get_the_content(), 18);
+                    }  
+                ?> 
                 <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
               </div>
             </div>
@@ -68,7 +74,15 @@
               </a>
               <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                <p>
+                <?php 
+                if(has_excerpt()) {
+                  echo get_the_excerpt();
+                  } else {
+                    echo wp_trim_words(get_the_content(), 18);
+                    }  
+                ?> 
+                <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
               </div>
             </div>
             <?php
