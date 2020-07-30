71006d0e0ab3a051462167156a1dc3839355913e
Khoi Hoang <hello@koicha.dev>
2020-07-30 18:27:18 +0200

    Add: :sparkles: Added the S, and ESC button on the keyboard to open/close overlay
    This feature will make sure we can save the CPU resource cost by using the keydown, instead of keyup, because  we use isOverlayOpen property to flag

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,25 +1,39 @@
 class Search {
-    // 1. describe and create/initiate our object
+    // 1. Describe and create/initiate our object
     constructor() {
         this.openButton = document.querySelector('.js-search-trigger');
         this.closeButton = document.querySelector('.search-overlay__close');
         this.searchOverlay = document.querySelector('.search-overlay');
+        this.body = document.querySelector('body');
         this.events();
+        this.isOverlayOpen = false; // We use this in context with keydown, to flag and reduce cost of the DOM CPU resource 
     }
 
-    // 2. events 
+    // 2. Event Listener
     events() {
         this.openButton.addEventListener('click', this.openOverlay.bind(this));
         this.closeButton.addEventListener('click', this.closeOverlay.bind(this));
+        // We use keydown instead of keyup. Keyup have to make sure user let go of the key to trigger the event, 
+        // so keydown is more is better UX. It always guarantee the s-key will always work
+        document.addEventListener('keydown', this.keyPressDispatcher.bind(this));
     }
 
-    // 3. methods
+    // 3. Methods trigger for the Event Listener
     openOverlay() {
         this.searchOverlay.classList.add('search-overlay--active');
+        this.body.classList.add('body-no-scroll');
+        this.isOverlayOpen = true;
     }
 
     closeOverlay() {
         this.searchOverlay.classList.remove('search-overlay--active');
+        this.body.classList.remove('body-no-scroll');
+        this.isOverlayOpen = false;
+    }
+
+    keyPressDispatcher(e) {
+        e.keyCode === 83 && !this.isOverlayOpen && this.openOverlay();
+        e.keyCode === 27 && this.isOverlayOpen && this.closeOverlay();
     }
 }
 
