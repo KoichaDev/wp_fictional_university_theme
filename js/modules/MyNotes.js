e9c1af2c79ffd1e07dfe0ba694adc10e50921f0d
Khoi Hoang <hello@koicha.dev>
2020-08-03 16:05:00 +0200

    Add/feature: :sparkles: :lipstick: The delete of the button will show real-time
    When the user click on the delete button. It will show on the real time through the jQuery animation of slideUp() function

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -21,7 +21,9 @@ class MyNotes {
         const li = e.target.parentElement;
 
         http.delete(kho_university_data.root_url + `/wp-json/wp/v2/note/${li.getAttribute('data-id')}`)
-            .then(data => console.log(data))
+            .then(data => {
+                $(li).slideUp();
+            })
             .catch(err => console.log(err));
     }
 }
