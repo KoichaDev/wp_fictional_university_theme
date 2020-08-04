3aa087eaa3f83fab0b23c3550c78673ee9cfdaa0
Khoi Hoang <hello@koicha.dev>
2020-08-04 17:58:39 +0200

    Update/feature: :art: Added the update HTTP
    Updated this library for update PUT http request

--- js/modules/HTTP.js
+++ js/modules/HTTP.js
@@ -1,4 +1,19 @@
 class HTTP {
+    // Make an HTTP Update Request
+    async update(url, data) {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json',
+                'X-WP-Nonce': kho_university_data.nonce
+            },
+            credentials: 'same-origin',
+            body: JSON.stringify(data)
+        })
+        return await response.json();
+    }
+
+
     // Make an HTTP DELETE Request
     async delete(url) {
         const response = await fetch(url, {
